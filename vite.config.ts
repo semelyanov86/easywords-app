3f4b39831e45b0b5a25dc98f39ee2c9ac6122698
Sergey Emelyanov <se@sergeyem.ru>
2026-01-18 16:28:12 +0100

    disable wayfinder

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
     ],
     esbuild: {
         jsx: 'automatic',
